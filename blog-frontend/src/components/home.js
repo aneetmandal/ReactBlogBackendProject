957fac1e3e8232fcb92bf3dc1e75329278e2fc8e
Aneet Mandal <aneet.mandal@gmail.com>
2023-04-17 21:00:17 +0530

    Added appropriate server site

--- blog-frontend/src/components/home.js
+++ blog-frontend/src/components/home.js
@@ -8,7 +8,7 @@ function Home(){
 
     // useEffect()
     const io = require("socket.io-client");
-    const socket = io("http://localhost:3001");
+    const socket = io("https://blogbackend-k5vd.onrender.com/");
     const navigate = useNavigate();
 
     // const goToHome = () =>{
